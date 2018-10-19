d4877e466a6c7f8698c4a0eb8f7d12721570b884
Jan Ladleif <jan.ladleif@hpi.uni-potsdam.de>
2018-10-19 11:22:40 +0200

    add render code for attached messages

--- app/custom-modeler/custom/CustomRenderer.js
+++ app/custom-modeler/custom/CustomRenderer.js
@@ -19,6 +19,9 @@ import {
 } from 'min-dash';
 
 let CHOREO_TASK_ROUNDING = 10;
+let MESSAGE_WIDTH = 35;
+let MESSAGE_HEIGHT = MESSAGE_WIDTH / 7 * 5;
+let MESSAGE_DISTANCE = 20;
 
 function translate(x, y, object) {
   let group = svgAttr(svgCreate('g'), {
@@ -26,6 +29,21 @@ function translate(x, y, object) {
   });
   svgAppend(group, object);
   return group;
+}
+
+function getEnvelopePath(width, height) {
+  let flap = height * 0.6;
+  let path = [
+    ['M', 0, 0],
+    ['l', 0, height],
+    ['l', width, 0],
+    ['l', 0, -height],
+    ['z']
+    ['M', 0, 0],
+    ['l', width / 2., flap],
+    ['l', width / 2., -flap]
+  ];
+  return componentsToPath(path);
 }
 
 function getTaskOutline(width, height) {
@@ -47,7 +65,6 @@ function getTaskOutline(width, height) {
 function getParticipantBandOutline(width, height, participantBandKind) {
   let path;
   let r = CHOREO_TASK_ROUNDING;
-  let bandDiff = height - r;
   participantBandKind = participantBandKind || 'top_initiating';
   if (participantBandKind.startsWith('top')) {
     path = [
@@ -107,19 +124,74 @@ export default function CustomRenderer(eventBus, styles, textRenderer) {
   }
 
   this.drawParticipantBand = function(p, element) {
-    var group = svgCreate('g');
+    let group = svgCreate('g');
+    let bandKind = element.diBand.participantBandKind || 'top-initiating';
+    let isBottom = bandKind.startsWith('bottom');
+    let isInitiating = !bandKind.endsWith('non_initiating');
 
-    var bandKind = element.diBand.participantBandKind;
-    var shape = svgCreate('path');
+    // optionally display an envelope for the attached message
+    if (element.diBand.isMessageVisible) {
+      // first, draw the connecting dotted line
+      let connector = svgCreate('path');
+      svgAttr(connector, {
+        d: componentsToPath([
+          ['M', element.width / 2, isBottom ? element.height : -MESSAGE_DISTANCE],
+          ['l', 0, MESSAGE_DISTANCE]
+        ]),
+        stroke: 'black',
+        strokeWidth: 2,
+        strokeDasharray: '0, 4',
+        strokeLinecap: 'round'
+      });
+      svgAppend(group, connector);
+
+      // then, draw the envelope
+      let envelope = svgCreate('path');
+      svgAttr(envelope, {
+        d: getEnvelopePath(MESSAGE_WIDTH, MESSAGE_HEIGHT),
+        stroke: '#000000',
+        strokeWidth: 2,
+        fill: isInitiating ? 'white' : 'lightgray',
+        fillOpacity: 1,
+      });
+      svgAppend(group, translate(
+        element.width / 2 - MESSAGE_WIDTH / 2,
+        isBottom ? element.height + MESSAGE_DISTANCE
+                 : -MESSAGE_DISTANCE - MESSAGE_HEIGHT,
+        envelope)
+      );
+
+      // add a label with the message name if applicable
+      // first, find the message flow with this participant as a source
+      let messageFlow = element.choreoActivity.businessObject.messageFlowRef
+                               .find(flow => flow.sourceRef === element.businessObject);
+      if (messageFlow && messageFlow.messageRef) {
+        let message = messageFlow.messageRef;
+        if (message.name) {
+          let label = getBoxedLabel(message.name, {
+            x: 0,
+            y: isBottom ? element.height + MESSAGE_DISTANCE + MESSAGE_HEIGHT
+                        : -MESSAGE_DISTANCE - MESSAGE_HEIGHT - element.height,
+            width: element.width,
+            height: element.height
+          }, 'center-middle');
+          svgAppend(group, label);
+        }
+      }
+    }
+
+    // draw the actual participant band
+    let shape = svgCreate('path');
     svgAttr(shape, {
       d: getParticipantBandOutline(element.width, element.height, bandKind),
       stroke: '#000000',
       strokeWidth: 2,
-      fill: (bandKind && bandKind.endsWith('non_initiating')) ? 'lightgray' : 'white',
+      fill: isInitiating ? 'white' : 'lightgray',
       fillOpacity: 1,
     });
     svgAppend(group, shape);
 
+    // add the name of the participant
     let label = getBoxedLabel(element.businessObject.name, {
       x: 0,
       y: 0,
@@ -193,6 +265,6 @@ CustomRenderer.prototype.getShapePath = function(shape) {
   if (type === 'bpmn:ChoreographyTask' || type === 'bpmn:SubChoreography') {
     return getTaskOutline(shape.width, shape.height)
   } else if (type === 'bpmn:Participant') {
-    //TODO return proper path for the participant band
+    return getParticipantBandOutline(shape.width, shape.height, shape.diBand.participantBandKind);
   }
 };
