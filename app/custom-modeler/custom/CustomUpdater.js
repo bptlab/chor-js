352f79a302d62abb2bf7be49bb8054d7c27cae7a
Jan Ladleif <jan.ladleif@hpi.uni-potsdam.de>
2018-10-22 17:06:21 +0200

    override bpmn updater to stop the update of participant bands

--- app/custom-modeler/custom/CustomUpdater.js
+++ app/custom-modeler/custom/CustomUpdater.js
@@ -1,56 +1,22 @@
 import inherits from 'inherits';
 
-import {
-  pick,
-  assign
-} from 'min-dash';
-
-import CommandInterceptor from 'diagram-js/lib/command/CommandInterceptor';
+import BpmnUpdater from 'bpmn-js/lib/features/modeling/BpmnUpdater';
 
 import {
   is
 } from 'bpmn-js/lib/util/ModelUtil';
-
-import {
-  add as collectionAdd,
-  remove as collectionRemove
-} from 'diagram-js/lib/util/Collections';
 
 
 /**
  * A handler responsible for updating the custom element's businessObject
  * once changes on the diagram happen.
  */
-export default function CustomUpdater(eventBus, bpmnjs) {
+export default function CustomUpdater(eventBus, bpmnFactory, connectionDocking, translate) {
 
-  CommandInterceptor.call(this, eventBus);
+  BpmnUpdater.call(this, eventBus, bpmnFactory, connectionDocking, translate);
 
   function updateChoreographyActivities(e) {
-    console.log('update choreo activities', e);
-  }
-
-  function updateCustomElement(e) {
-    var context = e.context,
-        shape = context.shape,
-        businessObject = shape.businessObject;
-
-    if (!isCustom(shape)) {
-      return;
-    }
-
-    var parent = shape.parent;
-
-    var customElements = bpmnjs._customElements;
-
-    // make sure element is added / removed from bpmnjs.customElements
-    if (!parent) {
-      collectionRemove(customElements, businessObject);
-    } else {
-      collectionAdd(customElements, businessObject);
-    }
-
-    // save custom element position
-    assign(businessObject, pick(shape, [ 'x', 'y' ]));
+    // do some specific updating for choreography activities
   }
 
   this.executed([
@@ -64,48 +30,37 @@ export default function CustomUpdater(eventBus, bpmnjs) {
     'shape.move',
     'shape.delete'
   ], ifChoreographyActivity(updateChoreographyActivities));
-
-  this.executed([
-    'shape.create',
-    'shape.move',
-    'shape.delete'
-  ], ifCustomElement(updateCustomElement));
-
-  this.reverted([
-    'shape.create',
-    'shape.move',
-    'shape.delete'
-  ], ifCustomElement(updateCustomElement));
 }
 
-inherits(CustomUpdater, CommandInterceptor);
+inherits(CustomUpdater, BpmnUpdater);
 
-CustomUpdater.$inject = [ 'eventBus', 'bpmnjs' ];
+CustomUpdater.$inject = [
+  'eventBus',
+  'bpmnFactory',
+  'connectionDocking',
+  'translate'
+];
 
+CustomUpdater.prototype.updateParent = function(element, oldParent) {
+  if (!is(element, 'bpmn:Participant')) {
+    BpmnUpdater.prototype.updateParent.call(this, element, oldParent);
+  }
+};
+
+CustomUpdater.prototype.updateSemanticParent = function(businessObject, newParent, visualParent) {
+  if (!is(businessObject, 'bpmn:Participant')) {
+    BpmnUpdater.prototype.updateSemanticParent.call(this, businessObject, newParent, visualParent);
+  }
+};
 
 /////// helpers ///////////////////////////////////
-
-function isCustom(element) {
-  return element && /custom:/.test(element.type);
-}
-
-function ifCustomElement(fn) {
-  return function(event) {
-    var context = event.context,
-        element = context.shape || context.connection;
-
-    if (isCustom(element)) {
-      fn(event);
-    }
-  };
-}
 
 function ifChoreographyActivity(fn) {
   return function(event) {
     var context = event.context,
         element = context.shape || context.connection;
 
-    if (is(element, 'bpmn:ChoreographtyActivity')) {
+    if (is(element, 'bpmn:ChoreographyActivity')) {
       fn(event);
     }
   };
