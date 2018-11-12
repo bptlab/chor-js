f20341252befab16aefa94e952220e7fe556d3eb
Anton von Weltzien <anton.weltzien@gmail.com>
2018-11-12 12:36:37 +0100

    Add first test and necessary test setup

--- test/spec/ChoreoModelerSpec.js
+++ test/spec/ChoreoModelerSpec.js
@@ -4,69 +4,40 @@ import TestContainer from 'mocha-test-container-support';
 
 import ChoreoModeler from '../../app/choreo-modeler';
 
-import {
-  is
-} from 'bpmn-js/lib/util/ModelUtil';
 
 
-describe('custom modeler', function() {
+describe('choreo modeler', function() {
 
-  //var xml = require('./diagram.bpmn');
-
-  var container;
+  let container;
 
   beforeEach(function() {
     container = TestContainer.get(this);
   });
 
+  function createModeler(xml, importDone) {
+
+    let modeler = new ChoreoModeler({
+      container: container
+    });
+
+    modeler.importXML(xml, (err, warnings) => importDone(modeler, err, warnings));
+  }
+
 
   describe('custom elements', function() {
-
-    var modeler;
-
-    // spin up modeler with custom element before each test
-    beforeEach(function(done) {
-
-      modeler = new ChoreoModeler({ container: container });
-
-      // modeler.importXML(xml, function(err) {
-      //   if (!err) {
-      //     done();
-      //   }
-      // });
-
-      done();
-
-    });
 
     it('should run', function() {
     });
 
-    xit('should import custom element', function() {
-
-      // given
-      var elementRegistry = modeler.get('elementRegistry'),
-          customElements = modeler.getCustomElements();
-
-      // when
-      var customElement = {
-        type: 'custom:triangle',
-        id: 'CustomTriangle_1',
-        x: 300,
-        y: 200
-      };
-
-      modeler.addCustomElements([ customElement ]);
-      var customTriangle = elementRegistry.get('CustomTriangle_1');
-
-      // then
-      expect(is(customTriangle, 'custom:triangle')).to.be.true;
-
-      expect(customTriangle).to.exist;
-      expect(customElements).to.contain(customElement);
-
+    it('should have correct choreo names', function(done) {
+      let xml = require('../../resources/tasksWithMultiplicities.bpmn');
+      createModeler(xml, function(modeler) {
+        let elementRegistry = modeler.get('elementRegistry');
+        let choreoTask_1 = elementRegistry.get('ChoreographyTask_1').businessObject;
+        expect(choreoTask_1.name).to.eql('Task 1');
+        done();
+      });
     });
-
   });
 
 });
