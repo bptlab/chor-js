c3c48798c73e7ea60c15431937027d32c9c1dc37
Anton von Weltzien <a.weltzien@outlook.de>
2018-11-19 16:34:51 +0100

    Add test for loop markers, choreo type and for participant. This commit will also add some documentation to the test setup

--- test/spec/ChoreoModelerSpec.js
+++ test/spec/ChoreoModelerSpec.js
@@ -5,8 +5,9 @@ import TestContainer from 'mocha-test-container-support';
 import ChoreoModeler from '../../app/choreo-modeler';
 
 
-
 describe('choreo modeler', function() {
+  const choreWithMultiplicities = require('../../resources/tasksWithMultiplicities.bpmn');
+  const choreoWithLoops = require('../../resources/tasksWithLoopType.bpmn');
 
   let container;
 
@@ -14,53 +15,139 @@ describe('choreo modeler', function() {
     container = TestContainer.get(this);
   });
 
+  /**
+   * Creates a new modeler and calls importDone with it after importing an xml diagram
+   * @param {string} xml bpmn diagram
+   * @param {function(ChoreoModeler, ElementRegistry, Error, string[])} importDone taking a modeler, an ElementRegistry,
+   * error, and warnings
+   */
   function createModeler(xml, importDone) {
 
-    let modeler = new ChoreoModeler({
+    const modeler = new ChoreoModeler({
       container: container
     });
 
-    modeler.importXML(xml, (err, warnings) => importDone(modeler, err, warnings));
+    const elementRegistry = modeler.get('elementRegistry');
+
+    modeler.importXML(xml, (err, warnings) => importDone(modeler, elementRegistry, err, warnings));
+  }
+
+  /**
+   * Creates a new modeler for each diagram and calls importDone with it after importing an xml diagram
+   * @param {function(ChoreoModeler, ElementRegistry, Error, string[])} importDone taking a modeler, an ElementRegistry,
+   * error, and warnings
+   */
+  function createModelerForEachDiagram(importDone) {
+    const allDiagrams = [choreoWithLoops, choreWithMultiplicities];
+    allDiagrams.forEach(xml => createModeler(xml, importDone));
   }
 
 
   describe('choreo import', function() {
-    const choreWithMultiplicities = require('../../resources/tasksWithMultiplicities.bpmn');
-    const choreoWithLoops = require('../../resources/tasksWithLoopType.bpmn');
 
     it.skip('should have no warnings on import of choreo with multiplicities', function(done) {
-      createModeler(choreWithMultiplicities, function(modeler, err, warnings) {
-        expect(warnings).to.have.lengthOf(0);
+      createModeler(choreWithMultiplicities, function(modeler, elemReg, err, warnings) {
+        expect(warnings).to.be.empty;
         done();
       });
     });
 
     it('should have no error on import of choreo with multiplicities', function(done) {
-      createModeler(choreWithMultiplicities, function(modeler, err, warnings) {
+      createModeler(choreWithMultiplicities, function(modeler, elemReg, err, warnings) {
         expect(err).to.be.undefined;
         done();
       });
     });
 
     it.skip('should have no warnings on import of choreo with loops', function(done) {
-      createModeler(choreoWithLoops, function(modeler, err, warnings) {
-        expect(warnings).to.have.lengthOf(0);
+      createModeler(choreoWithLoops, function(modeler, elemReg, err, warnings) {
+        expect(warnings).to.be.empty;
         done();
       });
     });
 
     it('should have no error on import of choreo with loops', function(done) {
-      createModeler(choreoWithLoops, function(modeler, err, warnings) {
+      createModeler(choreoWithLoops, function(modeler, elemReg, err, warnings) {
         expect(err).to.be.undefined;
         done();
       });
     });
 
     it('should have correct choreo names', function(done) {
-      createModeler(choreWithMultiplicities, function(modeler) {
-        let elementRegistry = modeler.get('elementRegistry');
-        let choreoTask_1 = elementRegistry.get('ChoreographyTask_1').businessObject;
+      createModeler(choreWithMultiplicities, function(modeler, elemReg) {
+        let choreoTask_1 = elemReg.get('ChoreographyTask_1').businessObject;
         expect(choreoTask_1.name).to.eql('Task 1');
+        done();
+      });
+    });
+
+    it('should have standard loop marker', function(done) {
+      createModeler(choreoWithLoops, function(modele, elemReg) {
+        const businessObject = elemReg.get('ChoreographyTask_2').businessObject;
+        expect(businessObject.loopType).to.equal('Standard');
+        const gfx = elemReg.getGraphics('ChoreographyTask_2');
+        const marker = gfx.querySelector('[data-marker=loop]');
+        expect(marker).to.exist;
+        done();
+      });
+    });
+
+    it('should have parallel loop marker', function(done) {
+      createModeler(choreoWithLoops, function(modele, elemReg) {
+        const businessObject = elemReg.get('ChoreographyTask_3').businessObject;
+        expect(businessObject.loopType).to.equal('MultiInstanceParallel');
+        const gfx = elemReg.getGraphics('ChoreographyTask_3');
+        const marker = gfx.querySelectorAll('[data-marker=parallel]');
+        expect(marker).to.have.lengthOf(1);
+        done();
+      });
+    });
+
+    it('should have sequential loop marker', function(done) {
+      createModeler(choreoWithLoops, function(modele, elemReg) {
+        const businessObject = elemReg.get('ChoreographyTask_4').businessObject;
+        expect(businessObject.loopType).to.equal('MultiInstanceSequential');
+        const gfx = elemReg.getGraphics('ChoreographyTask_4');
+        const marker = gfx.querySelectorAll('[data-marker=sequential]');
+        expect(marker).to.have.lengthOf(1);
+        done();
+      });
+    });
+
+    it('should have sequential loop marker', function(done) {
+      createModeler(choreoWithLoops, function(modele, elemReg) {
+        const businessObject = elemReg.get('ChoreographyTask_4').businessObject;
+        expect(businessObject.loopType).to.equal('MultiInstanceSequential');
+        const gfx = elemReg.getGraphics('ChoreographyTask_4');
+        const marker = gfx.querySelectorAll('[data-marker=sequential]');
+        expect(marker).to.have.lengthOf(1);
+        done();
+      });
+    });
+
+    it('should have correct bpmn:ChoreographyTask type for Choreo Tasks', function(done) {
+      createModeler(choreoWithLoops, function(modele, elemReg) {
+        const choreoTask = elemReg.get('ChoreographyTask_1');
+        expect(choreoTask.businessObject.$type).to.equal('bpmn:ChoreographyTask');
+        expect(choreoTask.type).to.equal('bpmn:ChoreographyTask');
+        done();
+      });
+    });
+
+    it('should have one initiating participant', function(done) {
+      createModeler(choreoWithLoops, function(modele, elemReg) {
+        const businessObject = elemReg.get('ChoreographyTask_1').businessObject;
+        expect(businessObject.initiatingParticipantRef).to.exist;
+        expect(businessObject.initiatingParticipantRef.$type).to.equal('bpmn:Participant');
+        done();
+      });
+    });
+
+    it('should have at least two participants for each ChoreoTask', function(done) {
+      createModelerForEachDiagram(function(modele, elemReg) {
+        elemReg.filter(shape => shape.type === 'bpmn:ChoreographyTask').forEach(
+          choreoTask => expect(choreoTask.businessObject.participantRefs, 'Participants').to.have.lengthOf.at.least(2)
+        );
         done();
       });
     });
