1c58654918aa1917cf9475a4d8262825567dd301
Jan Ladleif <jan-ladleif@web.de>
2018-11-20 15:02:24 +0100

    Remove properties panel (#34)

--- app/choreo-modeler/properties-panel/index.js
+++
@@ -1,6 +0,0 @@
-import ChoreoPropertiesProvider from './ChoreoPropertiesProvider';
-
-export default {
-  __init__: [ 'propertiesProvider' ],
-  propertiesProvider: [ 'type', ChoreoPropertiesProvider ]
-};
