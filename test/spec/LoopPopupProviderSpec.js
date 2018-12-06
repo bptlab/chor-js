b355eed07c989162b9bfb57baf652ce6351079f9
Anton von Weltzien <a.weltzien@outlook.de>
2018-12-06 12:48:18 +0100

    Change loop type (#50)
    * Allows creating a standard loop type marker by pressing on wrench icon.
    Can be undone.
    
    * Add initial implementation of popup menu for loop type selection. Loop types can be selected and are mutually elusive. Changes are stored in model and available in .bpmn file.
    
    * Create some initial tests for the loop type selection popup, plus some test
    stubs for test that are yet to be implemented.
    
    * Add more tests for loop type selection popup menu
    
    * Add basic documentation to tests.
    
    * minor review
    
    * extend to sub-choreos as well

---
+++ test/spec/LoopPopupProviderSpec.js
@@ -0,0 +1,128 @@
+import ChoreoModeler from '../../lib/Modeler';
+import { bootstrapBpmnJS as bootstrapChorJS, getBpmnJS as getChorJS, inject } from 'bpmn-js/test/helper/index';
+
+describe('loop popup provider', function() {
+
+  /**
+   * Invokes an action in the Popup menu
+   * @param popupMenu use inject to get the popup menu
+   * @param element to open the menu on
+   * @param loopType {string} that should be selected from menu
+   */
+  let invokeAction = function(popupMenu, element, loopType) {
+    popupMenu.open(element, 'loop-provider', { x: 0, y: 0 });
+    const entry = popupMenu._current.headerEntries.find(x => x.loopType === loopType);
+    entry.action(undefined, entry);
+  };
+
+  const choreoWithLoops = require('../resources/tasksWithLoopType.bpmn');
+
+  beforeEach(bootstrapChorJS(ChoreoModeler, choreoWithLoops));
+
+  it('should mark selected loop types as active', function(done) {
+    const elem = getChorJS().get('elementRegistry').get('ChoreographyTask_2');
+    inject(function(popupMenu) {
+      popupMenu.open(elem, 'loop-provider', { x: 0, y: 0 });
+      let entry = popupMenu._current.headerEntries.find(x => x.active);
+      expect(entry.loopType).to.equal('Standard');
+      done();
+    })();
+  });
+
+  it('should be closed on default', function(done) {
+    inject(function(popupMenu) {
+      expect(popupMenu.isOpen()).to.be.false;
+      done();
+    })();
+  });
+
+  it('can be opened', function(done) {
+    inject(function(popupMenu) {
+      const elem = getChorJS().get('elementRegistry').get('ChoreographyTask_2');
+      popupMenu.open(elem, 'loop-provider', { x: 0, y: 0 });
+      expect(popupMenu.isOpen()).to.be.true;
+      done();
+    })();
+  });
+
+  it('should toggle loop types exclusively', function(done) {
+    const elem = getChorJS().get('elementRegistry').get('ChoreographyTask_2');
+    inject(function(popupMenu) {
+      invokeAction(popupMenu, elem, 'MultiInstanceParallel');
+      invokeAction(popupMenu, elem, 'MultiInstanceSequential');
+      expect(popupMenu._current.headerEntries.filter(e => e.active)).to.have.length(1);
+      expect(elem.businessObject.loopType).to.equal('MultiInstanceSequential');
+      done();
+    })();
+  });
+
+  it('should create changes in .bpmn file', function(done) {
+    const none_task = getChorJS().get('elementRegistry').get('ChoreographyTask_1');
+    const standard_task = getChorJS().get('elementRegistry').get('ChoreographyTask_2');
+    const parallel_task = getChorJS().get('elementRegistry').get('ChoreographyTask_3');
+    const sequential_task = getChorJS().get('elementRegistry').get('ChoreographyTask_4');
+    inject(function(popupMenu) {
+      invokeAction(popupMenu, none_task, 'MultiInstanceSequential');
+      invokeAction(popupMenu, standard_task, 'MultiInstanceParallel');
+      invokeAction(popupMenu, parallel_task, 'Standard');
+      invokeAction(popupMenu, sequential_task, 'MultiInstanceSequential');
+    })();
+    getChorJS().saveXML({ format: true }, function(err, xml) {
+      expect(xml).to.have.string('<bpmn2:choreographyTask id="ChoreographyTask_1" name="Basic Task" ' +
+        'initiatingParticipantRef="Participant_1" loopType="MultiInstanceSequential">');
+      expect(xml).to.have.string('<bpmn2:choreographyTask id="ChoreographyTask_2" name="Standard Loop Task" ' +
+        'initiatingParticipantRef="Participant_1" loopType="MultiInstanceParallel">');
+      expect(xml).to.have.string('<bpmn2:choreographyTask id="ChoreographyTask_3" name="Parallel Loop Task" ' +
+        'initiatingParticipantRef="Participant_1" loopType="Standard">');
+      expect(xml).to.have.string('<bpmn2:choreographyTask id="ChoreographyTask_4" name="Sequential Loop Task" ' +
+        'initiatingParticipantRef="Participant_1">');
+      done();
+    });
+  });
+
+  it('should create Standard loop type', function(done) {
+    const elem = getChorJS().get('elementRegistry').get('ChoreographyTask_1');
+    inject(function(popupMenu) {
+      invokeAction(popupMenu, elem, 'Standard');
+      expect(elem.businessObject.loopType).to.equal('Standard');
+      done();
+    })();
+  });
+
+  it('should create Multi Instance Parallel loop type', function(done) {
+    const elem = getChorJS().get('elementRegistry').get('ChoreographyTask_1');
+    inject(function(popupMenu) {
+      invokeAction(popupMenu, elem, 'MultiInstanceParallel');
+      expect(elem.businessObject.loopType).to.equal('MultiInstanceParallel');
+      done();
+    })();
+  });
+
+  it('should create Multi Instance Sequential loop type', function(done) {
+    const elem = getChorJS().get('elementRegistry').get('ChoreographyTask_1');
+    inject(function(popupMenu) {
+      invokeAction(popupMenu, elem, 'MultiInstanceSequential');
+      expect(elem.businessObject.loopType).to.equal('MultiInstanceSequential');
+      done();
+    })();
+  });
+
+  it('should remove loop type', function(done) {
+    const standard_task = getChorJS().get('elementRegistry').get('ChoreographyTask_2');
+    inject(function(popupMenu) {
+      invokeAction(popupMenu, standard_task, 'Standard');
+      expect(standard_task.businessObject.loopType).to.equal('None');
+      done();
+    })();
+  });
+
+  it('should have three loop types selectable', function(done) {
+    const elem = getChorJS().get('elementRegistry').get('ChoreographyTask_1');
+    inject(function(popupMenu) {
+      popupMenu.open(elem, 'loop-provider', { x: 0, y: 0 });
+      expect(popupMenu._current.headerEntries.map(x => x.loopType)).to.have.members([
+        'MultiInstanceParallel', 'MultiInstanceSequential', 'Standard']);
+      done();
+    })();
+  });
+});
