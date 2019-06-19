170a8e6c320df1090edf3d5c96bd3faa4931701b
Christian Friedow <christian@friedow.com>
2019-06-19 15:59:48 +0200

    Choreography switch component (#117)
    * add module for choreography switcher
    
    * Added Choreography select element
    
    * Added Add choreography button
    
    * Added rename and delete command
    
    * Implemented AddChoreographyHandler
    
    * Used createChoreoHandler in Switch.js
    
    * Added and used minimal choreoUtil
    
    * Moved some functions to utils
    
    * Adjusted create handler to use util functions
    
    * Added choreography delete command handler
    
    * Added choreography rename command handler
    
    * Adjusted choreoUtil to reuse own functions
    
    * Refactored switch component to reduce dom manipulation
    
    * Fixed linting errors
    
    * Fixed PR issues
    
    * register handlers after rendering to avoid require loop
    
    * refactor choreography import logic and fix undo/redo functionality for choreo switcher
    
    * fix tests
    
    * some styling

--- test/spec/ChoreoModelerSpec.js
+++ test/spec/ChoreoModelerSpec.js
@@ -16,65 +16,36 @@ describe('choreo modeler', function() {
   });
 
   /**
-   * Creates a new modeler and calls importDone with it after importing an xml diagram
-   * @param {string} xml bpmn diagram
-   * @param {function(ChoreoModeler, ElementRegistry, Error, string[])} importDone taking a modeler, an ElementRegistry,
-   * error, and warnings
+   * Returns a promise resolving with a new modeler instance displaying the given XML diagram.
    */
-  function createModeler(xml, importDone) {
-
+  function createModeler(xml) {
     const modeler = new ChoreoModeler({
       container: container
     });
-
-    const elementRegistry = modeler.get('elementRegistry');
-
-    modeler.importXML(xml, (err, warnings) => importDone(modeler, elementRegistry, err, warnings));
+    return modeler.setXML(xml).then(res => {
+      return modeler.displayChoreography();
+    }).then(res => {
+      return modeler;
+    });
   }
-
-  /**
-   * Creates a new modeler for each diagram and calls importDone with it after importing an xml diagram
-   * @param {function(ChoreoModeler, ElementRegistry, Error, string[])} importDone taking a modeler, an ElementRegistry,
-   * error, and warnings
-   */
-  function createModelerForEachDiagram(importDone) {
-    const allDiagrams = [choreoWithLoops, choreoWithMultiplicities];
-    allDiagrams.forEach(xml => createModeler(xml, importDone));
-  }
-
 
   describe('choreo import', function() {
 
-    it('should have no warnings on import of choreo with multiplicities', function(done) {
-      createModeler(choreoWithMultiplicities, function(modeler, elemReg, err, warnings) {
-        expect(warnings).to.be.empty;
+    it('should not fail on import of choreo with multiplicities', function(done) {
+      createModeler(choreoWithMultiplicities).then(modeler => {
         done();
       });
     });
 
-    it('should have no error on import of choreo with multiplicities', function(done) {
-      createModeler(choreoWithMultiplicities, function(modeler, elemReg, err, warnings) {
-        expect(err).to.be.undefined;
+    it('should not fail on import of choreo with loops', function(done) {
+      createModeler(choreoWithLoops).then(modeler => {
         done();
       });
     });
 
-    it('should have no warnings on import of choreo with loops', function(done) {
-      createModeler(choreoWithLoops, function(modeler, elemReg, err, warnings) {
-        expect(warnings).to.be.empty;
-        done();
-      });
-    });
-
-    it('should have no error on import of choreo with loops', function(done) {
-      createModeler(choreoWithLoops, function(modeler, elemReg, err, warnings) {
-        expect(err).to.be.undefined;
-        done();
-      });
-    });
-
-    it('should have correct choreo names', function(done) {
-      createModeler(choreoWithMultiplicities, function(modeler, elemReg) {
+    it('should have correct task names', function(done) {
+      createModeler(choreoWithMultiplicities).then(modeler => {
+        const elemReg = modeler.get('elementRegistry');
         let choreoTask_1 = elemReg.get('ChoreographyTask_1').businessObject;
         expect(choreoTask_1.name).to.eql('Task 1');
         done();
@@ -82,7 +53,8 @@ describe('choreo modeler', function() {
     });
 
     it('should have standard loop marker', function(done) {
-      createModeler(choreoWithLoops, function(modeler, elemReg) {
+      createModeler(choreoWithLoops).then(modeler => {
+        const elemReg = modeler.get('elementRegistry');
         const businessObject = elemReg.get('ChoreographyTask_2').businessObject;
         expect(businessObject.loopType).to.equal('Standard');
         const gfx = elemReg.getGraphics('ChoreographyTask_2');
@@ -93,7 +65,8 @@ describe('choreo modeler', function() {
     });
 
     it('should have parallel loop marker', function(done) {
-      createModeler(choreoWithLoops, function(modeler, elemReg) {
+      createModeler(choreoWithLoops).then(modeler => {
+        const elemReg = modeler.get('elementRegistry');
         const businessObject = elemReg.get('ChoreographyTask_3').businessObject;
         expect(businessObject.loopType).to.equal('MultiInstanceParallel');
         const gfx = elemReg.getGraphics('ChoreographyTask_3');
@@ -104,7 +77,8 @@ describe('choreo modeler', function() {
     });
 
     it('should have sequential loop marker', function(done) {
-      createModeler(choreoWithLoops, function(modeler, elemReg) {
+      createModeler(choreoWithLoops).then(modeler => {
+        const elemReg = modeler.get('elementRegistry');
         const businessObject = elemReg.get('ChoreographyTask_4').businessObject;
         expect(businessObject.loopType).to.equal('MultiInstanceSequential');
         const gfx = elemReg.getGraphics('ChoreographyTask_4');
@@ -115,7 +89,8 @@ describe('choreo modeler', function() {
     });
 
     it('should have correct bpmn:ChoreographyTask type for Choreo Tasks', function(done) {
-      createModeler(choreoWithLoops, function(modeler, elemReg) {
+      createModeler(choreoWithLoops).then(modeler => {
+        const elemReg = modeler.get('elementRegistry');
         const choreoTask = elemReg.get('ChoreographyTask_1');
         expect(choreoTask.businessObject.$type).to.equal('bpmn:ChoreographyTask');
         expect(choreoTask.type).to.equal('bpmn:ChoreographyTask');
@@ -124,7 +99,8 @@ describe('choreo modeler', function() {
     });
 
     it('should have one initiating participant', function(done) {
-      createModeler(choreoWithLoops, function(modeler, elemReg) {
+      createModeler(choreoWithLoops).then(modeler => {
+        const elemReg = modeler.get('elementRegistry');
         const businessObject = elemReg.get('ChoreographyTask_1').businessObject;
         expect(businessObject.initiatingParticipantRef).to.exist;
         expect(businessObject.initiatingParticipantRef.$type).to.equal('bpmn:Participant');
@@ -133,7 +109,8 @@ describe('choreo modeler', function() {
     });
 
     it('should have exactly two participants for each ChoreoTask', function(done) {
-      createModelerForEachDiagram(function(modeler, elemReg) {
+      createModeler(choreoWithLoops).then(modeler => {
+        const elemReg = modeler.get('elementRegistry');
         elemReg.filter(shape => shape.type === 'bpmn:ChoreographyTask').forEach(
           choreoTask => expect(choreoTask.businessObject.participantRef, 'Participants').to.have.lengthOf(2)
         );
@@ -142,7 +119,7 @@ describe('choreo modeler', function() {
     });
 
     it('should export xml without error', function(done) {
-      createModelerForEachDiagram(function(modeler, elemReg) {
+      createModeler(choreoWithLoops).then(modeler => {
         modeler.saveXML({ format: true }, function(err, xml) {
           expect(err).to.be.undefined;
         });
