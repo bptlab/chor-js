099a797a53b7528a224384adb8c0f40c005a351d
Anton von Weltzien <a.weltzien@outlook.de>
2020-06-12 18:02:51 +0200

    fix global export. Fix warning during tests for missing browserify which karma needs but cant install itself :/ remove old demo html for fonts, update npm ignore, add prepublish to package.json

--- bundler.js
+++ bundler.js
@@ -12,22 +12,17 @@ const entryFiles = [
     name: 'chor-js-viewer',
     path: './lib/Viewer.js'
   }];
-// Single entrypoint file location:
-// OR: Multiple files with globbing (can also be .js)
-// const entryFiles = './src/*.js';
-// OR: Multiple files in an array
-// const entryFiles = ['./src/index.html', './some/other/directory/scripts.js'];
 
-// Bundler options
+// Bundler options, some of theme are set do default values just to be sure and more verbose
 const base_options = {
-  hmr: false,
+  hmr: false, // Disable hot module reload. it is not needed here.
   outDir: './dist', // The out directory to put the build files in, defaults to dist
   watch: false, // Whether to watch the files and rebuild them on change, defaults to process.env.NODE_ENV !== 'production'
-  cache: true, // Enabled or disables caching, defaults to true
+  cache: true, // Enabled or disables caching.
   contentHash: false, // Disable content hash from being included on the filename
   minify: false, // Minify files, enabled if process.env.NODE_ENV === 'production'
-  //target: 'browser', // Browser/node/electron, defaults to browser//
-  logLevel: 4, // 5 = save everything to a file, 4 = like 3, but with timestamps and additionally log http requests to dev server, 3 = log info, warnings & errors, 2 = log warnings & errors, 1 = log errors, 0 = log nothing
+  target: 'browser', // Browser/node/electron, defaults to browser//
+  logLevel: 2, // 5 = save everything to a file, 4 = like 3, but with timestamps and additionally log http requests to dev server, 3 = log info, warnings & errors, 2 = log warnings & errors, 1 = log errors, 0 = log nothing
   sourceMaps: true, // Enable or disable sourcemaps, defaults to enabled (minified builds currently always create sourcemaps)
   detailedReport: true, // Prints a detailed report of the bundles, assets, filesizes and times, defaults to false, reports are only printed if watch is disabled
   autoInstall: true, // Enable or disable auto install of missing dependencies found during bundling
@@ -47,19 +42,21 @@ async function bundleCSS() {
   // Initializes a bundler using the entrypoint location and options provided
   const file = 'assets/styles/chor-js.css';
   const cssOptions = {
-    ...base_options, outFile: 'chor-js.css', outDir: './dist/assets', publicUrl: './' // for relative paths
+    ...base_options,
+    outFile: 'chor-js.css',
+    outDir: './dist/assets',
+    publicUrl: './' // for relative paths in css
   };
   const bundler = new Bundler(file, cssOptions);
 
-  // Run the bundler, this returns the main bundle
-  // Use the events if you're using watch mode as this promise will only trigger once and not for every rebuild
   await bundler.bundle();
 }
 
 async function bundleAll() {
   for (const entrypoint of entryFiles) {
+
     const entryFile = Path.join(__dirname, entrypoint.path);
-    console.log(entryFile);
+    console.log('Bundling development version of: ' + entryFile);
     const devOptions = {
       ...base_options,
       outFile: entrypoint.name + '.development.js'
@@ -67,6 +64,7 @@ async function bundleAll() {
     await bundleChorJs(entryFile, devOptions);
     console.log('Bundled: ' + devOptions.outFile);
 
+    console.log('Bundling minified version of: ' + entryFile);
     const minOptions = {
       ...base_options,
       outFile: entrypoint.name + '.min.js',
@@ -78,4 +76,4 @@ async function bundleAll() {
   await bundleCSS();
 }
 
-bundleAll().then(_ => console.log('Done!'));
+bundleAll().then(_ => console.log('Done bundling!'));
